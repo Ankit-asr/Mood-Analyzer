a3867bfc3fff8f51889cbc3011bcdaf5fd781700
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-14 12:34:37 +0530

    [Ankit] Add: Implemented Test case 3.1 ,given null mood should throw mood analysis exception

--- MoodAnalyzerTesting/UnitTest1.cs
+++ MoodAnalyzerTesting/UnitTest1.cs
@@ -1,31 +1,45 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MoodAnalyzer;
-
-namespace MoodAnalyzerTesting
+using MoodAnalyserProblem;
+using System;
+namespace MoodAnalyserTesting
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
+        ///<summery>
+        ///Test1.1 message should return Sad
+        ///<summery>
         public void GivenSadMessage_WhenAnalyse_ShouldReturnSad()
         {
-            MoodAnalyser mood = new MoodAnalyser();
-            string actual = mood.AnalyseMood();
+            MoodAnalysers moodAnalyser = new MoodAnalysers("I am in sad mood");
+            string actual = moodAnalyser.AnalyserMood();
             Assert.AreEqual("Sad", actual);
         }
         [TestMethod]
+        ///<summery>
+        ///Test1.2 message should return Sad or Happy
+        ///<summery>
         public void GivenMessage_WhenAnalyse_ShouldReturnSadOrHappy()
         {
-            MoodAnalyser mood = new MoodAnalyser();
-            string actual = mood.AnalyseMood();
+            MoodAnalysers moodAnalyser = new MoodAnalysers("I am in any Mood");
+            string actual = moodAnalyser.AnalyserMood();
             Assert.AreEqual("Happy", actual);
         }
         [TestMethod]
+        ///<summary>
+        ///Test 2.1 If message is null throw exception and return Happy
+        ///<summary>
+        ///Test 3.1 message should return mood is null or mood is empty
+        /// </summary>
         public void GivenNullMessage_WhenAnalyseShouldReturnException()
         {
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-            string actual = moodAnalyser.AnalyseMood();
+            MoodAnalysers moodAnalyser = new MoodAnalysers();
+            string actual = moodAnalyser.AnalyserMood();
             Assert.AreEqual("Happy", actual);
+            MoodAnalysers moodAnalysers = new MoodAnalysers();
+            string actual = moodAnalysers.AnalyserMood();
+            Assert.AreEqual("mood is null", actual);
         }
     }
 }
